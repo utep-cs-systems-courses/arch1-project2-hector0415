700d3d3bc756ea46c9782a251c3ad8a751d37a3b
HectorVM <hareyes2@miners.utep.edu>
2020-03-17 22:37:30 -0600

    add .c and .h files for led, stateMachines, and switches.

---
+++ project/buzzer.c
@@ -0,0 +1,23 @@
+#include "util.h"
+
+void buzzer_init()
+{
+  /*
+    Direct timer A output "TA0.1" to P2.6.
+     According to table 21 from data sheet:
+       P2SEL2.6, P2SEL2.7, and P2SEL.7 must be zero
+       P2SEL2.6 must be 1
+     Also: P2.6 directtion must be output
+  */
+  timerAUpmode(); /*Used to drive speaker */
+  P2SEL2 &= ~(BIT6 | BIT7);
+  P2SEL &= ~BIT7;
+  P2SEL |= BIT6;
+  P2DIR = BIT6;
+}
+
+void buzzer_set_period(short cycles) /*buzzer clock = 2MHz. (Period of 1k results in 2kHz tone) */
+{
+  CCR0 = cycles;
+  CCR1 = cycles >> 1;   /* one half cycle */
+}
