b604f10ff390a37cae467f2cfef617defd48999d
HectorVM <hareyes2@miners.utep.edu>
2020-03-17 22:38:28 -0600

    Added centralized header file that contains all header files needed

---
+++ project/util.h
@@ -0,0 +1,12 @@
+#ifndef utilities_included
+#define utilities_included
+
+#include "buzzer.h"
+#include "led.h"
+#include "stateMachines.h"
+#include "switches.h"
+
+#include "libTimer.h"
+#include <msp430.h>
+
+#endif // included
