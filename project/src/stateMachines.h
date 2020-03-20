a4ab7859f804e63fb5ae40fc2b58a87045a104e7
HectorVM <hareyes2@miners.utep.edu>
2020-03-20 17:00:01 -0600

    Added code for stateMachines.h

---
+++ project/src/stateMachines.h
@@ -0,0 +1,16 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+extern char state;
+
+void state_advance();
+
+void state_one();
+
+void state_two();
+
+void state_three();
+
+void state_four();
+
+#endif
